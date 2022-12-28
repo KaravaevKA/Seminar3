c4e9db204b02caf94b0eee0f7c984327c1c24b9f
Kirill Karavaev <coffee.2016@yandex.ru>
2022-12-28 21:03:29 +0300

    Добавлена задача 18

---
+++ Zadacha18/Program.cs
@@ -0,0 +1,32 @@
+﻿string quarter(int x)
+{
+    if (x == 1)
+    {
+        string x_="x>0 и y>0";
+        return x_;
+    }
+    else if (x == 2)
+    {
+        string x_="x<0 и y>0";
+        return x_;
+    }
+    else if (x == 3)
+    {
+        string x_="x<0 и y<0";
+        return x_;
+    }
+    else if (x == 4)
+    {
+        string x_="x>0 и y<0";
+        return x_;
+    }
+    else
+    {
+        string x_="Области не существует";
+        return x_;
+    }
+}
+
+Console.WriteLine("Введите номер четверти");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(quarter(num));
