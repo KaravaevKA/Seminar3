a58e39a26633c3859117312fdbcba9fa87a5ae79
Kirill Karavaev <coffee.2016@yandex.ru>
2022-12-29 20:28:14 +0300

    Добавлена задача 21 из дз

---
+++ Zadacha21/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите X1");
+int x1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите Y1");
+int y1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите Z1");
+int z1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите X2");
+int x2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите Y2");
+int y2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите Z2");
+int z2 = int.Parse(Console.ReadLine());
+double d = Math.Sqrt(Math.Pow(x1-x2, 2) + Math.Pow(y1-y2, 2) + Math.Pow(z1-z2, 2));
+
+Console.WriteLine($"d={d:f2}");
