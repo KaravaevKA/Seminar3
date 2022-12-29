a0fa8f76ee5235afb40e97a8e1726fad4b17fa26
Kirill Karavaev <coffee.2016@yandex.ru>
2022-12-29 19:16:47 +0300

    добавлена задача 23 из дз

---
+++ Zadacha23/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число");
+int Num = Convert.ToInt32(Console.ReadLine());
+int index = 1;
+while (index <= Num)
+{
+    Console.WriteLine(index*index*index);
+    index++;
+}
